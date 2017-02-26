30735d920ad829a37710364f65c09a475c541b72
jacho29 <jacho29@gmail.com>
2017-02-26 22:45:07 +0100

    Add files via upload

---
+++ Commander/Commander/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Commander
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdKopiuj = new System.Windows.Forms.Button();
+            this.cmdUsun = new System.Windows.Forms.Button();
+            this.cmdPrzenies = new System.Windows.Forms.Button();
+            this.lvOkno1 = new System.Windows.Forms.ListView();
+            this.lvOkno2 = new System.Windows.Forms.ListView();
+            this.Dysk1 = new System.Windows.Forms.ComboBox();
+            this.Dysk2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cmdKopiuj
+            // 
+            this.cmdKopiuj.Location = new System.Drawing.Point(30, 319);
+            this.cmdKopiuj.Name = "cmdKopiuj";
+            this.cmdKopiuj.Size = new System.Drawing.Size(127, 58);
+            this.cmdKopiuj.TabIndex = 4;
+            this.cmdKopiuj.Text = "KOPIUJ\r\n   F6\r\n";
+            this.cmdKopiuj.UseVisualStyleBackColor = true;
+            this.cmdKopiuj.Click += new System.EventHandler(this.cmdKopiuj_Click);
+            // 
+            // cmdUsun
+            // 
+            this.cmdUsun.Location = new System.Drawing.Point(188, 319);
+            this.cmdUsun.Name = "cmdUsun";
+            this.cmdUsun.Size = new System.Drawing.Size(127, 58);
+            this.cmdUsun.TabIndex = 5;
+            this.cmdUsun.Text = "USUŃ\r\n   F7";
+            this.cmdUsun.UseVisualStyleBackColor = true;
+            this.cmdUsun.Click += new System.EventHandler(this.cmdUsun_Click);
+            // 
+            // cmdPrzenies
+            // 
+            this.cmdPrzenies.Location = new System.Drawing.Point(353, 319);
+            this.cmdPrzenies.Name = "cmdPrzenies";
+            this.cmdPrzenies.Size = new System.Drawing.Size(127, 58);
+            this.cmdPrzenies.TabIndex = 8;
+            this.cmdPrzenies.Text = "PRZENIEŚ\r\n    F8";
+            this.cmdPrzenies.UseVisualStyleBackColor = true;
+            this.cmdPrzenies.Click += new System.EventHandler(this.cmdPrzenies_Click);
+            // 
+            // lvOkno1
+            // 
+            this.lvOkno1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lvOkno1.FullRowSelect = true;
+            this.lvOkno1.HideSelection = false;
+            this.lvOkno1.Location = new System.Drawing.Point(30, 55);
+            this.lvOkno1.Name = "lvOkno1";
+            this.lvOkno1.Size = new System.Drawing.Size(202, 232);
+            this.lvOkno1.TabIndex = 9;
+            this.lvOkno1.TileSize = new System.Drawing.Size(220, 15);
+            this.lvOkno1.UseCompatibleStateImageBehavior = false;
+            this.lvOkno1.View = System.Windows.Forms.View.Tile;
+            this.lvOkno1.Enter += new System.EventHandler(this.lvOkno1_Enter);
+            this.lvOkno1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lvOkno_KeyDown);
+            this.lvOkno1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvOkno_MouseDoubleClick);
+            // 
+            // lvOkno2
+            // 
+            this.lvOkno2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvOkno2.FullRowSelect = true;
+            this.lvOkno2.HideSelection = false;
+            this.lvOkno2.Location = new System.Drawing.Point(277, 55);
+            this.lvOkno2.Name = "lvOkno2";
+            this.lvOkno2.Size = new System.Drawing.Size(203, 232);
+            this.lvOkno2.TabIndex = 10;
+            this.lvOkno2.TileSize = new System.Drawing.Size(220, 15);
+            this.lvOkno2.UseCompatibleStateImageBehavior = false;
+            this.lvOkno2.View = System.Windows.Forms.View.Tile;
+            this.lvOkno2.Enter += new System.EventHandler(this.lvOkno2_Enter);
+            this.lvOkno2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lvOkno_KeyDown);
+            this.lvOkno2.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvOkno_MouseDoubleClick);
+            // 
+            // Dysk1
+            // 
+            this.Dysk1.FormattingEnabled = true;
+            this.Dysk1.Location = new System.Drawing.Point(30, 12);
+            this.Dysk1.Name = "Dysk1";
+            this.Dysk1.Size = new System.Drawing.Size(121, 21);
+            this.Dysk1.TabIndex = 11;
+            this.Dysk1.SelectedIndexChanged += new System.EventHandler(this.Dysk1_SelectedIndexChanged);
+            // 
+            // Dysk2
+            // 
+            this.Dysk2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Dysk2.FormattingEnabled = true;
+            this.Dysk2.Location = new System.Drawing.Point(277, 12);
+            this.Dysk2.Name = "Dysk2";
+            this.Dysk2.Size = new System.Drawing.Size(121, 21);
+            this.Dysk2.TabIndex = 12;
+            this.Dysk2.SelectedIndexChanged += new System.EventHandler(this.Dysk2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 413);
+            this.Controls.Add(this.Dysk2);
+            this.Controls.Add(this.Dysk1);
+            this.Controls.Add(this.lvOkno2);
+            this.Controls.Add(this.lvOkno1);
+            this.Controls.Add(this.cmdPrzenies);
+            this.Controls.Add(this.cmdUsun);
+            this.Controls.Add(this.cmdKopiuj);
+            this.Name = "Form1";
+            this.Text = "Commander by Piotr Jaśkiewicz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdKopiuj;
+        private System.Windows.Forms.Button cmdUsun;
+        private System.Windows.Forms.Button cmdPrzenies;
+        private System.Windows.Forms.ListView lvOkno1;
+        private System.Windows.Forms.ListView lvOkno2;
+        private System.Windows.Forms.ComboBox Dysk1;
+        private System.Windows.Forms.ComboBox Dysk2;
+    }
+}
+
---
+++ Commander/Commander/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Commander
+{
+    public partial class Form1 : Form
+    {
+        private ListView AktywneOkno = null;
+        private string AktywnaSciezka;
+        private ListView NieAktywneOkno = null;
+        private string NieAktywnaSciezka;
+        private string sciezka1 = "C:\\";
+        private string sciezka2 = "C:\\";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void wyswietl(ListView gdzie, string katalog)
+        {
+            gdzie.Items.Clear();
+            string[] nazwy;
+            ListViewItem buf;
+            FileInfo plik;
+            DirectoryInfo dir;
+            if (!Dysk1.Items.Contains(katalog))
+                gdzie.Items.Add(new ListViewItem("..."));
+
+            int i;
+            nazwy = Directory.GetDirectories(katalog);
+            for (i = 0; i < nazwy.Length; i++)
+            {
+                dir = new DirectoryInfo(nazwy[i]);
+                buf = new ListViewItem(new string[] { dir.Name, "<DIR>" });
+                gdzie.Items.Add(buf);
+            }
+            string nazwapliku;
+            int gdziekropka;
+            nazwy = Directory.GetFiles(katalog);
+            for (i = 0; i < nazwy.Length; i++)
+            {
+                plik = new FileInfo(nazwy[i]);
+                nazwapliku = plik.Name;
+                gdziekropka = plik.Name.LastIndexOf('.');
+                if (gdziekropka > 0)
+                    nazwapliku = nazwapliku.Substring(0, plik.Name.LastIndexOf('.'));
+                buf = new ListViewItem(new string[] { nazwapliku, plik.Extension.Replace(".", ""),
+                    plik.Length.ToString() });
+                gdzie.Items.Add(buf);
+            }
+        }
+
+        private void KopiujKatalog(string zrodlo, string cel)
+        {
+            string[] nazwy;
+            if (!Directory.Exists(cel)) Directory.CreateDirectory(cel);
+            else if (zrodlo.Contains(cel)) MessageBox.Show("Błąd kopiowania", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+        //    else MessageBox.Show("Błąd kopiowania", "Błąd",
+        //           MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            nazwy = Directory.GetFileSystemEntries(zrodlo);
+            foreach (string nazwa in nazwy)
+            {
+                if (Directory.Exists(nazwa))
+                {
+                    KopiujKatalog(nazwa, cel + Path.GetFileName(nazwa) + "\\");
+                }
+                else
+                {
+                    File.Copy(nazwa, cel + Path.GetFileName(nazwa), true);
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] nazwy;
+            nazwy = Directory.GetLogicalDrives();
+            foreach (string dysk in nazwy)
+            {
+                Dysk1.Items.Add(dysk);
+                Dysk2.Items.Add(dysk);
+            }
+            Dysk1.SelectedIndex = Dysk1.Items.IndexOf("C:\\");
+            Dysk2.SelectedIndex = Dysk2.Items.IndexOf("C:\\");
+        }
+        private void Dysk1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sciezka1 = Dysk1.Text;
+            try
+            {
+                wyswietl(lvOkno1, sciezka1);
+            }
+            catch
+            {
+                MessageBox.Show("Błąd przy próbie dostępu do napędu", "Błąd",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Dysk1.SelectedIndex = Dysk1.Items.IndexOf("C:\\");
+            }
+
+        }
+
+        private void Dysk2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sciezka2 = Dysk2.Text;
+            try
+            {
+                wyswietl(lvOkno2, sciezka2);
+            }
+            catch
+            {
+                MessageBox.Show("Błąd przy próbie dostępu do napędu", "Błąd",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Dysk2.SelectedIndex = Dysk2.Items.IndexOf("C:\\");
+            }
+        }
+
+        private void lvOkno1_Enter(object sender, EventArgs e)
+        {
+            AktywneOkno = lvOkno1;
+            AktywnaSciezka = sciezka1;
+            NieAktywneOkno = lvOkno2;
+            NieAktywnaSciezka = sciezka2;
+            
+
+        }
+
+        private void lvOkno2_Enter(object sender, EventArgs e)
+        {
+            AktywneOkno = lvOkno2;
+            AktywnaSciezka = sciezka2;
+            NieAktywneOkno = lvOkno1;
+            NieAktywnaSciezka = sciezka1;
+   //         label1.Text = ;
+        }
+
+        private void lvOkno_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (AktywneOkno.SelectedItems[0].Text == "...")
+                {
+                    AktywnaSciezka = AktywnaSciezka.TrimEnd(new char[] { '\\' }); // usuniecie ostatniego znaku '\'
+                    int last; // indeks ostatniego znaku '\'
+                    last = AktywnaSciezka.LastIndexOf('\\');
+                    AktywnaSciezka = AktywnaSciezka.Substring(0, last + 1);
+                    wyswietl(AktywneOkno, AktywnaSciezka);
+                }
+                else if (AktywneOkno.SelectedItems[0].SubItems[1].Text == "<DIR>")
+                {
+                    AktywnaSciezka += (AktywneOkno.SelectedItems[0].Text + "\\");
+                    wyswietl(AktywneOkno, AktywnaSciezka);
+                }
+                else
+                {
+                    string nazwa = AktywnaSciezka +
+                    AktywneOkno.SelectedItems[0].Text +
+                        "." + AktywneOkno.SelectedItems[0].SubItems[1].Text;
+                    try
+                    {
+                        System.Diagnostics.Process.Start(nazwa);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Nie można otworzyć pliku", "Błąd",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    return;
+                }
+                if (AktywneOkno == lvOkno1)
+                    sciezka1 = AktywnaSciezka;
+                else sciezka2 = AktywnaSciezka;
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.F6:
+                    cmdKopiuj_Click(null, null);
+                    break;
+                case Keys.F7:
+                    cmdUsun_Click(null, null);
+                    break;
+                case Keys.F8:
+                    cmdPrzenies_Click(null, null);
+                    break;
+            }
+        }
+
+        private void lvOkno_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (AktywneOkno.SelectedItems[0].Text == "...")
+                {
+                    AktywnaSciezka = AktywnaSciezka.TrimEnd(new char[] { '\\' });
+                    int last;
+                    last = AktywnaSciezka.LastIndexOf('\\');
+                    AktywnaSciezka = AktywnaSciezka.Substring(0, last + 1);
+                    wyswietl(AktywneOkno, AktywnaSciezka);
+                }
+                else if (AktywneOkno.SelectedItems[0].SubItems[1].Text == "<DIR>")
+                {
+                    AktywnaSciezka += (AktywneOkno.SelectedItems[0].Text + "\\");
+                    wyswietl(AktywneOkno, AktywnaSciezka);
+                }
+                else
+                {
+                    string nazwa = AktywnaSciezka +
+                        AktywneOkno.SelectedItems[0].Text +
+                        "." + AktywneOkno.SelectedItems[0].SubItems[1].Text;
+                    try
+                    {
+                        System.Diagnostics.Process.Start(nazwa);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Nie można otworzyć pliku", "Błąd",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    return;
+                }
+                if (AktywneOkno == lvOkno1)
+                    sciezka1 = AktywnaSciezka;
+                else sciezka2 = AktywnaSciezka;
+            }
+        }
+
+    
+
+        private void cmdKopiuj_Click(object sender, EventArgs e)
+        {
+            if (AktywneOkno == null) return;
+            for (int i = 0; i < AktywneOkno.SelectedItems.Count; i++)
+            {
+                if (AktywneOkno.SelectedItems[i].Text == "...")
+                    return;
+                string zrodlo, cel;
+                if (AktywneOkno.SelectedItems[i].SubItems[1].Text == "<DIR>")
+                {
+                    zrodlo = AktywnaSciezka + AktywneOkno.SelectedItems[i].Text + "\\";
+                    cel = NieAktywnaSciezka + AktywneOkno.SelectedItems[i].Text + "\\";
+                    KopiujKatalog(zrodlo, cel);
+                }
+                else
+                {
+                    zrodlo = AktywnaSciezka + AktywneOkno.SelectedItems[i].Text +
+                        "." + AktywneOkno.SelectedItems[i].SubItems[1].Text;
+                    cel = NieAktywnaSciezka + AktywneOkno.SelectedItems[i].Text +
+                        "." + AktywneOkno.SelectedItems[i].SubItems[1].Text;
+                    if (zrodlo.Contains(cel)) MessageBox.Show("Błąd kopiowania", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    File.Copy(zrodlo, cel, true);
+                }
+            }
+            wyswietl(NieAktywneOkno, NieAktywnaSciezka);
+        }
+
+        private void cmdPrzenies_Click(object sender, EventArgs e)
+        {
+            if (AktywneOkno == null) return;
+            for (int i = 0; i < AktywneOkno.SelectedItems.Count; i++)
+            {
+                if (AktywneOkno.SelectedItems[i].Text == "...")
+                    return;
+                string zrodlo, cel;
+                if (AktywneOkno.SelectedItems[i].SubItems[1].Text == "<DIR>")
+                {
+                    zrodlo = AktywnaSciezka + AktywneOkno.SelectedItems[i].Text + "\\";
+                    cel = NieAktywnaSciezka + AktywneOkno.SelectedItems[i].Text + "\\";
+                    KopiujKatalog(zrodlo, cel);
+                    Directory.Delete(zrodlo, true);
+                }
+                else
+                {
+                    zrodlo = AktywnaSciezka + AktywneOkno.SelectedItems[i].Text +
+                        "." + AktywneOkno.SelectedItems[i].SubItems[1].Text;
+                    cel = NieAktywnaSciezka + AktywneOkno.SelectedItems[i].Text +
+                        "." + AktywneOkno.SelectedItems[i].SubItems[1].Text;
+                    File.Copy(zrodlo, cel, true);
+                    File.Delete(zrodlo);
+                }
+            }
+            wyswietl(lvOkno1, sciezka1);
+            wyswietl(lvOkno2, sciezka2);
+        }
+
+        private void cmdUsun_Click(object sender, EventArgs e)
+        {
+            if (AktywneOkno == null) return;
+            if (AktywneOkno.SelectedItems[0].Text == "...")
+                return;
+            DialogResult odp = MessageBox.Show("Czy na pewno?", "Usuń",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (odp != DialogResult.Yes) return;
+            string co;
+            for (int i = 0; i < AktywneOkno.SelectedItems.Count; i++)
+            {
+                if (AktywneOkno.SelectedItems[i].SubItems[1].Text == "<DIR>")
+                {
+                    co = AktywnaSciezka + AktywneOkno.SelectedItems[i].Text;
+                    Directory.Delete(co, true);
+                }
+                else
+                {
+                    co = AktywnaSciezka + AktywneOkno.SelectedItems[i].Text +
+                        "." + AktywneOkno.SelectedItems[i].SubItems[1].Text;
+                    File.Delete(co);
+                }
+            }
+            wyswietl(AktywneOkno, AktywnaSciezka);
+        }
+
+
+    }
+}
